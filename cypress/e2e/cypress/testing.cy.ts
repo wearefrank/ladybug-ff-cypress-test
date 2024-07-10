1046ee915af586d52b9e9d8d957707a5384776d8
Martijn Dirkse <martijn@integrationpartners.nl>
2024-07-10 13:00:37 +0200

    Fix previous commit

--- cypress/e2e/cypress/testing.cy.ts
+++ cypress/e2e/cypress/testing.cy.ts
@@ -17,7 +17,12 @@ describe('Test user stories about testing with Ladybug', () => {
             cy.wrap($cell).click()
           }
         })
-      cy.getIframeBody().find('[data-cy-debug-tree="root"]').contains('Pipeline Adapter1a').should('have.length', 2)
+      cy.getIframeBody().find('[data-cy-debug-tree="root"]')
+        .contains('Pipeline Adapter1a').within(_ => {
+          cy.contains('Pipeline Adapter1a').within(_ => {
+            cy.contains('Pipeline Adapter1a').should('not.exist')
+          })
+        })
       cy.guardedCopyReportToTestTab('apiCopyTheReportToTestTab')
       cy.checkTestTabHasReportNamed('Pipeline Adapter1a').as('testTabReportRow')
         .find('[data-cy-test="runReport"]').click()
