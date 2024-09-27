cff67d308bcfae219a3ec93ba0170f74b1bae682
Martijn Dirkse <mhdirkse@live.nl>
2024-09-27 11:44:39 +0200

    Update names of checkpoints to follow behavior of system under test (#22)
    Co-authored-by: Martijn Dirkse <martijn@integrationpartners.nl>

--- cypress/e2e/cypress/testing.cy.ts
+++ cypress/e2e/cypress/testing.cy.ts
@@ -11,19 +11,22 @@ describe('Test user stories about testing with Ladybug', () => {
 
   it('Run report', () => {
     cy.createReportInLadybug('Example1a', 'Adapter1a', 'xxx').then(storageId => {
+      cy.wrap('Found report just created, storageId=' + storageId)
       cy.getIframeBody().find('[data-cy-debug="tableRow"]')
         .find('td:nth-child(2)').each($cell => {
           if (parseInt($cell.text()) === storageId) {
+            cy.wrap('Going to click cell with text' + $cell.text())
             cy.wrap($cell).click()
           }
         })
       cy.getIframeBody().find('[data-cy-debug-tree="root"]')
-        .contains('Pipeline Adapter1a').within(_ => {
-          cy.contains('Pipeline Adapter1a')
+        .should('have.length.at.least', 1)
+        .contains('Pipeline Example1a/Adapter1a').within(_ => {
+          cy.contains('Pipeline Example1a/Adapter1a')
           // TODO: Check that there is no 'Pipeline Adapter1a' inside of this anymore
         })
       cy.guardedCopyReportToTestTab('apiCopyTheReportToTestTab')
-      cy.checkTestTabHasReportNamed('Pipeline Adapter1a').as('testTabReportRow')
+      cy.checkTestTabHasReportNamed('Pipeline Example1a_Adapter1a').as('testTabReportRow')
         .find('[data-cy-test="runReport"]').click()
       // TODO: Use data-cy. This can only be done if there is a data-cy* tag to request
       cy.get('@testTabReportRow').find('td:eq(5)').should('contain', 'stubbed')
