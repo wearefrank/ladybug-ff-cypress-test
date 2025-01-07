28da23144435e081bb013ae49b2577aebc6425bd
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-07 20:13:36 +0100

    Restore Cypress tests as intended originally

--- cypress/e2e/cypress/common-over-test-envs/show-test-environment.cy.ts
+++ cypress/e2e/cypress/common-over-test-envs/show-test-environment.cy.ts
@@ -11,18 +11,6 @@ describe('Information to check the test environment', () => {
     })
   })
 
-  it('Check that the extra view defined in ladybug-ff-test-webapp exists', () => {
-    // cy.visit('/iaf/ladybug')
-    // cy.get('[data-cy-change-view-dropdown]').select('White box view with less metadata')
-    cy.visit('')
-    cy.wait(3000)
-    cy.getNumLadybugReports().then(() => {
-      cy.getIframeBody().find('[data-cy-change-view-dropdown]').select('White box view with less metadata')
-      // Wait so that you see this situation in the video
-      cy.wait(3000)
-    })
-  })
-
   it('See property ibistesttool.custom', () => {
     cy.visit('')
     cy.contains('Environment Variables').click()
---
+++ cypress/e2e/cypress/default/check-test-environment.cy.ts
@@ -0,0 +1,8 @@
+describe('Check test environment WITHOUT test webapp', () => {
+  it('Check that the extra view defined in ladybug-ff-test-webapp DOES NOT exist', () => {
+    cy.visit('/iaf/ladybug')
+    cy.get('[data-cy-change-view-dropdown]').find('White box view').should('exist')
+    cy.get('[data-cy-change-view-dropdown]').find('White box view with less metadata').should('not.exist')
+    cy.wait(3000)
+  })
+})
