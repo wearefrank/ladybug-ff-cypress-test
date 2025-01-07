5c029077e1b37c0dca2abe1bd7f4fc280a5c98a7
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-07 15:28:24 +0100

    With ladybug-ff-test-webapp, do not pop out Ladybug

--- cypress/e2e/cypress/with-ladybug-ff-test-webapp/check-test-environment.cy.ts
+++ cypress/e2e/cypress/with-ladybug-ff-test-webapp/check-test-environment.cy.ts
@@ -1,6 +1,10 @@
 describe('Check test environment', () => {
   it('Check that the extra view defined in ladybug-ff-test-webapp exists', () => {
-    cy.visit('/iaf/ladybug')
-    cy.get('[data-cy-change-view-dropdown]').select('White box view with less metadata')
+    // cy.visit('/iaf/ladybug')
+    // cy.get('[data-cy-change-view-dropdown]').select('White box view with less metadata')
+    cy.visit('')
+    cy.getNumLadybugReports().then(() => {
+      cy.getIframeBody().find('[data-cy-change-view-dropdown]').select('White box view with less metadata')
+    })
   })
 })
