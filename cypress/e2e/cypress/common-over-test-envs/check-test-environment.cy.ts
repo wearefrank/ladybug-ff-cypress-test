6bed74ccabca457f61101fd0926bde477699e9f7
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-07 16:41:58 +0100

    Try to move the offending test to another folder

--- cypress/e2e/cypress/common-over-test-envs/check-test-environment.cy.ts
+++ cypress/e2e/cypress/common-over-test-envs/check-test-environment.cy.ts
@@ -1,5 +1,5 @@
-describe('Check test environment', () => {
-  xit('Check that the extra view defined in ladybug-ff-test-webapp exists', () => {
+describe('Check test environment for use test webapp', () => {
+  it('Check that the extra view defined in ladybug-ff-test-webapp exists', () => {
     // cy.visit('/iaf/ladybug')
     // cy.get('[data-cy-change-view-dropdown]').select('White box view with less metadata')
     cy.visit('')
