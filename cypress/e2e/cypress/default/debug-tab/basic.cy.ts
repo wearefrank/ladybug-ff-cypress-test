6391cc69e31eb37b1451d9f2cf77ec8d5a8b4b46
Martijn Dirkse <martijn@integrationpartners.nl>
2025-05-15 11:16:58 +0200

    First draft of testing rerun in debug tab

--- cypress/e2e/cypress/default/debug-tab/basic.cy.ts
+++ cypress/e2e/cypress/default/debug-tab/basic.cy.ts
@@ -22,4 +22,28 @@ describe('Basic tests', () => {
       })
     })
   })
+
+  it('Rerun in debug tab', () => {
+    cy.apiDeleteAll(Cypress.env('debugStorageName') as string)
+    cy.apiDeleteAll('Test')
+    cy.visit('')
+    cy.createReportInLadybug('Example1a', 'Adapter1a', 'xxx').then(storageId => {
+      cy.wrap('Found report just created, storageId=' + storageId)
+      cy.inIframeBody('[data-cy-debug="tableRow"]')
+        .find('td:nth-child(2)').each($cell => {
+          if (parseInt($cell.text()) === storageId) {
+            cy.wrap('Going to click cell with text' + $cell.text())
+            cy.wrap($cell).click()
+          }
+        })
+      cy.inIframeBody('[data-cy-debug-tree="root"]')
+        .should('have.length.at.least', 1)
+        .contains('Pipeline Example1a/Adapter1a').within(_ => {
+          cy.contains('Pipeline Example1a/Adapter1a').click()
+        })
+      cy.inIframeBody('[datay-cy-report="rerun"]').click()
+      cy.inIframeBody(':contains(Save and rerun)').click()
+      cy.inIframeBody('.rerun-result').should('contain', 'stubbed')
+    })
+  })
 })
