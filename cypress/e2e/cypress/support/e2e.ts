9fe791d450431f1f7c833e871aa0d17d3b3a6c43
Martijn Dirkse <mhdirkse@pm.me>
2025-12-05 12:05:02 +0100

    Ignore app errors with message containing ResizeObserver

--- cypress/e2e/cypress/support/e2e.ts
+++ cypress/e2e/cypress/support/e2e.ts
@@ -1,1 +1,11 @@
 import './commands'
+
+Cypress.on('uncaught:exception', (err) => {
+  // Ignore ResizeObserver errors
+  if (err.message.includes('ResizeObserver')) {
+    return false; // prevents Cypress from failing the test
+  }
+
+  // Let other errors fail the test
+  return true;
+});
