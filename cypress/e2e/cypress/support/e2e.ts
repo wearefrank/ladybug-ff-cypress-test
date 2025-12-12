91076c06cd3c721aa6437e5fd3e6f2f7fb18b6e6
Martijn Dirkse <mhdirkse@pm.me>
2025-12-12 17:11:42 +0100

    Ignore the exception

--- cypress/e2e/cypress/support/e2e.ts
+++ cypress/e2e/cypress/support/e2e.ts
@@ -1,1 +1,9 @@
 import './commands'
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // ignore monaco-editor uncaught exceptions (thanks microsoft)
+  if (err.name === 'TypeError' && err.message === 'Property descriptor must be an object, got undefined') {
+    return false;
+  }
+  return;
+})
