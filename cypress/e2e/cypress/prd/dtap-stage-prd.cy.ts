0337b1685bc915e2c770789d331dadafa5c965fd
Martijn Dirkse <mhdirkse@pm.me>
2025-12-16 19:53:07 +0100

    Use non-empty message in test pipeline as workaround for internal server error

--- cypress/e2e/cypress/prd/dtap-stage-prd.cy.ts
+++ cypress/e2e/cypress/prd/dtap-stage-prd.cy.ts
@@ -28,7 +28,7 @@ describe('dtap.stage=PRD', () => {
       cy.apiDeleteAllAsTester('Test')
       cy.enterLadybug()
       cy.enableReportGenerator()
-      cy.createReportInLadybug('Example1a', 'Adapter1a', undefined, 'tester', 'IbisTester')
+      cy.createReportInLadybug('Example1a', 'Adapter1a', 'xxx', 'tester', 'IbisTester')
     })
 
     const credentialsToTest: Array<{ username: string, pwd: string }> = [
